da2fdab76394f55af656319e0877520d4313b0c2
Step30 <step30@INDCHN-2090Techopss-MBP.local>
2019-12-13 10:49:31 +0530

    intentation
    modifiy the intentation of program

--- src/line.js
+++ src/line.js
@@ -1,21 +1,21 @@
 const compareObject = function(object1, object2) {
-	return object1.x == object2.x && object1.y == object2.y;
+  return object1.x == object2.x && object1.y == object2.y;
 };
 
 class Line {
-	constructor(endA, endB) {
-		this.endA = endA;
-		this.endB = endB;
-	}
-	toString() {
-		return `line : (${this.endA.x},${this.endA.y}) and (${this.endB.x},${this.endB.y})`;
-	}
-	isEqualTo(providedLine) {
-		return (
-			compareObject(this.endA, providedLine.endA) &&
-			compareObject(this.endB, providedLine.endB)
-		);
-	}
+  constructor(endA, endB) {
+    this.endA = endA;
+    this.endB = endB;
+  }
+  toString() {
+    return `line : (${this.endA.x},${this.endA.y}) and (${this.endB.x},${this.endB.y})`;
+  }
+  isEqualTo(providedLine) {
+    return (
+      compareObject(this.endA, providedLine.endA) &&
+      compareObject(this.endB, providedLine.endB)
+    );
+  }
 }
 
 module.exports = Line;
