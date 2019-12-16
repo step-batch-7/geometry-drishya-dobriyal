4ea3bbf8ac6be1d7797ca79983d26db54c9bc06f
Step30 <step30@INDCHN-2090Techopss-MBP.local>
2019-12-16 14:25:25 +0530

    added test cases for isEqualTo in circle

--- test/testForCircle.js
+++ test/testForCircle.js
@@ -10,10 +10,25 @@ describe("circle", function() {
   });
 
   describe("isEqual", function() {
-    it("should give true if they are of same instance and same loc and radius", function() {
+    it("should give true if they are of same instance and same location and radius", function() {
       const circle = new Circle({ x: 0, y: 0 }, 5);
       const circle1 = new Circle({ x: 0, y: 0 }, 5);
       assert.ok(circle.isEqual(circle1));
+    });
+    it("should give false if they are of same instance ,same location and but different radius", function() {
+      const circle = new Circle({ x: 0, y: 0 }, 5);
+      const circle1 = new Circle({ x: 0, y: 0 }, 6);
+      assert.isFalse(circle.isEqual(circle1));
+    });
+    it("should give false if they are of same instance , same radius and diff location", function() {
+      const circle = new Circle({ x: 3, y: 0 }, 5);
+      const circle1 = new Circle({ x: 0, y: 0 }, 5);
+      assert.isFalse(circle.isEqual(circle1));
+    });
+    it("should give false if they are of same instance and same location and but different instance", function() {
+      const circle = new Circle({ x: 3, y: 0 }, 5);
+      const circle1 = ({ x: 0, y: 0 }, 5);
+      assert.isFalse(circle.isEqual(circle1));
     });
   });
 });
