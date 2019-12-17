8fe22f97d1b07a6a1a44da7ec5bd5184ce5712e8
Drishya Dobriyal <dobriyaldrishya@gmail.com>
2019-12-17 13:58:08 +0530

    added isEqualTo methoed to rectangle class

--- src/rectangle.js
+++ src/rectangle.js
@@ -1,12 +1,20 @@
 const Point = require("./point.js");
+const Line = require("./line.js");
 
 class Rectangle {
   constructor(endA, endC) {
     this.endA = new Point(endA.x, endA.y);
     this.endC = new Point(endC.x, endC.y);
+    this.diagonal = new Line(endA, endC);
   }
   toString() {
     return `[Rectangle (${this.endA.x},${this.endA.y}) to (${this.endC.x},${this.endC.y})]`;
+  }
+  isEqualTo(otherShape) {
+    return (
+      otherShape instanceof Rectangle &&
+      otherShape.diagonal.isEqualTo(this.diagonal)
+    );
   }
 }
 
--- test/testForRectangle.js
+++ test/testForRectangle.js
@@ -8,4 +8,17 @@ describe("rectangle", function() {
       assert.strictEqual(rectangle.toString(), "[Rectangle (1,1) to (2,3)]");
     });
   });
+
+  describe("isEqualTo", function() {
+    it("should give true if other shape is instance of the rectangle and have same co - ordinates as the rectangle", function() {
+      const rectangle1 = new Rectangle({ x: 1, y: 1 }, { x: 2, y: 3 });
+      const rectangle2 = new Rectangle({ x: 1, y: 1 }, { x: 2, y: 3 });
+      assert.isOk(rectangle1.isEqualTo(rectangle2));
+    });
+    it("should give false if other shape not an instance of the rectangle but have same co - ordinates as the rectangle", function() {
+      const rectangle1 = new Rectangle({ x: 1, y: 1 }, { x: 2, y: 3 });
+      const rectangle2 = ({ x: 1, y: 1 }, { x: 2, y: 3 });
+      assert.isFalse(rectangle1.isEqualTo(rectangle2));
+    });
+  });
 });
